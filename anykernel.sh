dd7cceb3d447ddfad4fb1241b5162c7a558f0b25
YOGRaJ <84258191+yograjfire18@users.noreply.github.com>
2024-05-17 03:54:34 +0000

    Update script for spartan

---
+++ anykernel.sh
@@ -0,0 +1,45 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=perf by YOGRaJ
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX3371
+device.name2=RE54E4L1
+device.name3=spartan
+device.name4=
+device.name5=
+supported.versions=13-14
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+vbmeta_disable_verification;
+
+write_boot;
+
