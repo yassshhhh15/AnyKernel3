7a7575cad7f0014b45422642f2386b7e6b9cbb4a
yassshhhh15 <yashpednekar264@gmail.com>
2025-01-25 18:38:39 +0000

    Update anykernel.sh & banner

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KEQING-OOS by yassshhhh15
+kernel.string=Keqing Kernel by yassshhhh15
 do.devicecheck=1
 do.modules=0
 do.systemless=1
