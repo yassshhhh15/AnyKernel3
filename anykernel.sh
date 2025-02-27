cbad2146de1b8d9a9fe5e6eec04bf8896d6a2ef0
yassshhhh15 <yashpednekar264@gmail.com>
2025-02-27 21:19:52 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Aurora Kernel by yassshhhh15
+kernel.string=Custom Kernel by yassshhhh15
 do.devicecheck=1
 do.modules=0
 do.systemless=1
